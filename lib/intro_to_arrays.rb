ceffa471dc2b318a841a9883abbec9732a835b84
apocalypse575 <apocalypse575@gmail.com>
2017-12-12 12:31:40 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -19,7 +19,7 @@ def last_element(array)
   return array[-1]
 end
 
-def first_element_with_array_meth(array)
+def first_element_with_array_methods(array)
   first = array.first
   return first
 end
