883ce45696a85d7ab9924ad10f364f87def384da
apocalypse575 <apocalypse575@gmail.com>
2017-12-12 12:09:02 +0000

    automatically backed up by learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,49 @@
+def instantiate_new_array
+  return []
+end
+
+#   rspec ./spec/intro_to_arrays_spec.rb:14 # working with arrays instantiating #instantiate_new_array
+# creates a new array and returns it
+
+# rspec ./spec/intro_to_arrays_spec.rb:18 # working with arrays instantiating #instantiate_new_array
+# returns an array that is empty
+
+# rspec ./spec/intro_to_arrays_spec.rb:29 # working with arrays instantiating #array_with_two_elements
+# creates a new array with two elements in it and returns that array
+
+# rspec ./spec/intro_to_arrays_spec.rb:33 # working with arrays instantiating #array_with_two_elements
+# returns an array that has 2 objects in it
+
+# rspec ./spec/intro_to_arrays_spec.rb:46 # working with arrays indexing #first_element
+# takes in an argument of an array and returns the first element
+#  in the array using its positive index
+
+# rspec ./spec/intro_to_arrays_spec.rb:53 # working with arrays indexing #third_element
+# takes in an argument of an array and returns the third element
+#  in the array using its positive index
+
+# rspec ./spec/intro_to_arrays_spec.rb:60 # working with arrays indexing #last_element
+# takes in an argument of an array and returns the last element i
+# n the array using its negative index
+
+# rspec ./spec/intro_to_arrays_spec.rb:53 # working with arrays indexing #third_element
+# takes in an argument of an array and returns the third element
+#  in the array using its positive index
+
+# rspec ./spec/intro_to_arrays_spec.rb:60 # working with arrays indexing #last_element
+# takes in an argument of an array and returns the last element i
+# n the array using its negative index
+
+# rspec ./spec/intro_to_arrays_spec.rb:73 # working with arrays using ruby array methods
+# to return values from an array #first_element_with_array_meth
+# ods takes in an argument of an array and returns the first element in the array,
+# without referencing the index number of that element
+
+# rspec ./spec/intro_to_arrays_spec.rb:80 # working with arrays using ruby array
+# methods to return values from an array #last_element_with_array_metho
+# ds takes in an argument of an array and returns the last element in the array,
+# wihtout referencing the index number
+
+# rspec ./spec/intro_to_arrays_spec.rb:93 # working with arrays using ruby array
+# methods to get information about an array #length_of_array takes in a
+# n argrument of an array and returns the length of the array
